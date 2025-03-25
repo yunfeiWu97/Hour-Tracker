e60cb44460e5086534ac63db358b2093d4f95162
yunfeiWu <wuyunfei97@gmail.com>
2025-03-24 22:06:56 -0500

    Complete the preliminary static code.

--- screens/HourlyRateScreen.js
+++ screens/HourlyRateScreen.js
@@ -1,4 +1,4 @@
-=import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
